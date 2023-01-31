8a2933f68ce57be2b2c3f9fd568ab8a1ab740b15
Ash-004 <ash_004@icloud.com>
2023-01-31 21:44:20 +0530

    Initial commit

---
+++ 5-passing-props-to-a-component/challenge-2.js
@@ -0,0 +1,42 @@
+import { getImageUrl } from './utils.js';
+
+function Avatar({ person, size }) {
+    let thumbnailSize = '';
+    if (size > 90) {
+        thumbnailSize = 'b';
+    }
+    else {
+        thumbnailSize = 's'
+    }
+
+    return (
+        <img
+            className="avatar"
+            src={getImageUrl(person, thumbnailSize)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+    );
+}
+
+export default function Profile() {
+    return (
+        <>
+            <Avatar
+                size={40}
+                person={{
+                    name: 'Gregorio Y. Zara',
+                    imageId: '7vQD0fP'
+                }}
+            />
+            <Avatar
+                size={120}
+                person={{
+                    name: 'Gregorio Y. Zara',
+                    imageId: '7vQD0fP'
+                }}
+            />
+        </>
+    );
+}
